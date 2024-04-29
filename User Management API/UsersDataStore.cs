2f53f54e5029d47b9c741d786cc2ef0e8a795250
Mark Bueno <133892804+mk17bu@users.noreply.github.com>
2024-04-29 11:21:39 +0200

    Fixed missing users list in DataStore

--- User Management API/UsersDataStore.cs
+++ User Management API/UsersDataStore.cs
@@ -1,47 +1,51 @@
 ﻿using User_Management_API.Models;
 
-namespace User_Management_API;
-
-public class UsersDataStore
+namespace User_Management_API
 {
-    public List<UserDto> Users { get; set; }
-
-    private UsersDataStore()
+    public class UsersDataStore
     {
-        new UserDto()
+        public List<UserDto> Users { get; set; }
+
+        public UsersDataStore()
         {
-            Id = 1,
-            FirstName = "Emily",
-            LastName = "Johnson",
-            Mail = "emily.johnson@example.com"
-        };
-        new UserDto()
-        {
-            Id = 2,
-            FirstName = "Alexander",
-            LastName = "Smith",
-            Mail = "alexander.smith@example.com"
-        };
-        new UserDto()
-        {
-            Id = 3,
-            FirstName = "Olivia",
-            LastName = "Williams",
-            Mail = "olivia.williams@example.com"
-        };
-        new UserDto()
-        {
-            Id = 4,
-            FirstName = "Ethan",
-            LastName = "Brown",
-            Mail = "ethan.brown@example.com"
-        };
-        new UserDto()
-        {
-            Id = 5,
-            FirstName = "Sophia",
-            LastName = "Davis",
-            Mail = "sophia.davis@example.com"
-        };
+            Users = new List<UserDto>()
+            {
+                new UserDto()
+                {
+                    Id = 1,
+                    FirstName = "Emily",
+                    LastName = "Johnson",
+                    Mail = "emily.johnson@example.com"
+                },
+                new UserDto()
+                {
+                    Id = 2,
+                    FirstName = "Alexander",
+                    LastName = "Smith",
+                    Mail = "alexander.smith@example.com"
+                },
+                new UserDto()
+                {
+                    Id = 3,
+                    FirstName = "Olivia",
+                    LastName = "Williams",
+                    Mail = "olivia.williams@example.com"
+                },
+                new UserDto()
+                {
+                    Id = 4,
+                    FirstName = "Ethan",
+                    LastName = "Brown",
+                    Mail = "ethan.brown@example.com"
+                },
+                new UserDto()
+                {
+                    Id = 5,
+                    FirstName = "Sophia",
+                    LastName = "Davis",
+                    Mail = "sophia.davis@example.com"
+                }
+            };
+        }
     }
 }

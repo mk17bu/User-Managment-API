34e29370a80e3e98c44bd2760445956caebf287a
mk17b <mk17bu@gmail.com>
2024-05-10 13:56:35 +0200

    Added seed data and validation rules

--- User Management API/DbContexts/UserManagementContext.cs
+++ User Management API/DbContexts/UserManagementContext.cs
@@ -15,11 +15,33 @@ namespace User_Management_API.DbContexts
             modelBuilder.Entity<User>(entity =>
             {
                 entity.Property(u => u.FirstName)
-                    .IsRequired().HasMaxLength(20);
+                    .IsRequired()
+                    .HasMaxLength(20);
+                
+                entity.Property(u => u.LastName)
+                    .HasMaxLength(20);
 
                 entity.Property(u => u.Mail)
                     .IsRequired()
                     .HasMaxLength(40);
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.Property(p => p.Title)
+                    .IsRequired()
+                    .HasMaxLength(40);
+                
+                entity.Property(p => p.Content)
+                    .IsRequired()
+                    .HasMaxLength(1000);
+            });
+            
+            modelBuilder.Entity<Comment>(entity =>
+            {
+                entity.Property(p => p.Content)
+                    .IsRequired()
+                    .HasMaxLength(500);
             });
             
             modelBuilder.Entity<User>().HasData(
@@ -29,7 +51,76 @@ namespace User_Management_API.DbContexts
                 new User { Id = 4, FirstName = "Joshua", LastName = "Kimmich", Mail = "joshuakimmich@fcb.com"},
                 new User { Id = 5, FirstName = "Robert", LastName = "Lewandoski", Mail = "robertlewandoski@fcb.com"}
             );
-
+            
+            modelBuilder.Entity<Post>().HasData(
+                new Post 
+                { 
+                    Id = 1, 
+                    Title = "Welcome Joshua Kimmich!", 
+                    Content = "Joshua Kimmich has officially joined FC Barcelona.",
+                    Date = DateTime.Now.AddHours(-2),
+                    UserId = 1 
+                },
+                new Post 
+                {
+                    Id = 2, 
+                    Title = "Hello team!",
+                    Content = "I'm very happy to be here and I'll try my best to fit the team.",
+                    Date = DateTime.Now.AddHours(-1),
+                    UserId = 4
+                }
+            );
+            
+            modelBuilder.Entity<Comment>().HasData(
+                new Comment
+                {
+                    Id = 1,
+                    PostId = 1,
+                    Content = "Nicee",
+                    Date = DateTime.Now.AddHours(-2),
+                    UserId = 2
+                },
+                new Comment
+                {
+                    Id = 2,
+                    PostId = 2,
+                    Content = "I'll teach you how to defend!",
+                    Date = DateTime.Now.AddHours(-1),
+                    UserId = 3
+                },
+                new Comment
+                {
+                    Id = 3,
+                    PostId = 2,
+                    Content = "Let's gooo!",
+                    Date = DateTime.Now,
+                    UserId = 5
+                }
+            );
+            
+            modelBuilder.Entity<Reaction>().HasData(
+                new Reaction
+                {
+                    Id = 1,
+                    PostId = 1,
+                    Type = ReactionType.Like,
+                    UserId = 2
+                },
+                new Reaction
+                {
+                    Id = 2,
+                    PostId = 2,
+                    Type = ReactionType.Like,
+                    UserId = 3
+                },
+                new Reaction
+                {
+                    Id = 3,
+                    PostId = 2,
+                    Type = ReactionType.Heart,
+                    UserId = 5
+                }
+            );
             base.OnModelCreating(modelBuilder);
         }
     }
